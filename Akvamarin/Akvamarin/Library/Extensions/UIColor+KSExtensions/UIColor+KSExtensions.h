c9ef473ec3eb38ced4ca3470e0e50e663de42237
KulikovS <sexadmiral@gmail.com>
2016-08-08 18:42:26 +0300

    CKCalendarKit color changed KSCalendarViewController refactor

---
+++ Akvamarin/Akvamarin/Library/Extensions/UIColor+KSExtensions/UIColor+KSExtensions.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+KSExtensions.h
+//  Akvamarin
+//
+//  Created by KulikovS on 06.08.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (KSExtensions)
+
+@end
---
+++ Akvamarin/Akvamarin/Library/Extensions/UIColor+KSExtensions/UIColor+KSExtensions.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+KSExtensions.m
+//  Akvamarin
+//
+//  Created by KulikovS on 06.08.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import "UIColor+KSExtensions.h"
+
+@implementation UIColor (KSExtensions)
+
+@end
